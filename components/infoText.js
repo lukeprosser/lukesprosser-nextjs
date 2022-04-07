b9fff9e686d9ae84b0da2124a389b5854000793c
Luke Prosser <lukesprosser@gmail.com>
2022-04-07 08:57:05 +0100

    fix infotext component and format posts

--- components/infoText.js
+++ components/infoText.js
@@ -1,6 +1,5 @@
 export default function InfoText({ children }) {
-  const {
-    props: { children: text },
-  } = children;
-  return <p className='p-6 bg-yellow-200 border-2'>{text}</p>;
+  return (
+    <p className='p-6 bg-yellow-200 border-2'>{children.props.children}</p>
+  );
 }
