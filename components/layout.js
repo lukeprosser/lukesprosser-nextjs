36505445884704060d96364f8122beb70093b308
Luke Prosser <lukesprosser@gmail.com>
2022-04-11 20:09:28 +0100

    force footer to bottom

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,9 @@ import Footer from './footer';
 
 export default function Layout({ children }) {
   return (
-    <div className='max-w-lg px-10 mx-auto sm:max-w-2xl md:max-w-4xl lg:max-w-6xl'>
+    // Force footer with 'flex flex-col h-screen'
+    // Corresponding 'flex-grow' on <main> in global styles
+    <div className='flex flex-col h-screen max-w-lg px-10 mx-auto sm:max-w-2xl md:max-w-4xl lg:max-w-6xl'>
       <Header />
       {children}
       <Footer />
